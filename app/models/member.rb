10566a12d9311815017a8326e879404c12e0f186
Dena Sawyer <denasawyer1111@gmail.com>
2015-07-31 18:26:30 -0400

    Fix email validater

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,39 +1,33 @@
 class Member < ActiveRecord::Base
 	validates_presence_of :name, :email, :photo
   belongs_to :company
-  validates :email, uniqueness: true, format: { with: /regex/i }
+  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates_length_of :name, minimum: 3
 
 
-	# has_attached_file :photo, styles: { medium: "300x300>", small: "150x150>", thumb: "100x100>" }
-	
-	# validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
-
-	has_attached_file :photo,
-  	:storage => :s3,
-  	:s3_credentials => Proc.new{|a| a.instance.s3_credentials },
+  has_attached_file :photo,
+    :storage => :s3,
+    :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
   	# :path => ":rails_root/public/system/:attachment/:id/:basename_:style.:extension",
   	:path => '/:class/:id/:style/:filename',
   	# :url => "/system/:attachment/:id/:basename_:style.:extension",
   	:url =>':s3_domain_url',
 
-
   	:styles => {
-   	 	:small    => ['150x150#',  :jpg, :quality => 70],
+      :small    => ['150x150#',  :jpg, :quality => 70],
       :medium    => ['300x300#',  :jpg, :quality => 70],
-    	:thumb    => ['100x100#',  :jpg, :quality => 70],
-    	:preview  => ['500x500#',  :jpg, :quality => 70],
-  	},
+      :thumb    => ['100x100#',  :jpg, :quality => 70],
+      :preview  => ['500x500#',  :jpg, :quality => 70],
+      },
 
-  	:convert_options => {
-    	:small    => '-set colorspace sRGB -strip',
-      :medium    => '-set colorspace sRGB -strip',
-    	:thumb    => '-set colorspace sRGB -strip',
-    	:preview  => '-set colorspace sRGB -strip',
-  	}
+    :convert_options => {
+       :small    => '-set colorspace sRGB -strip',
+       :medium    => '-set colorspace sRGB -strip',
+       :thumb    => '-set colorspace sRGB -strip',
+       :preview  => '-set colorspace sRGB -strip',
+     }
 
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-
 
 
   def s3_credentials
